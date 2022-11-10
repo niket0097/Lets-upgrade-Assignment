76ffd583126afac0a617dfd9f0410a4bcbee77a8
niket0097 <113575394+niket0097@users.noreply.github.com>
2022-11-11 01:20:01 +0530

    Add files via upload
    C++ program to find greater of 2 numbers

---
+++ LU_if_else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	int a,b;
+	cout<<"Enter the two numbers to compare\n";
+	cin>>a>>b;
+	
+	if(a>b)
+	 cout<<a<<" is greater than "<<b;
+	else
+	 cout<<b<<" is greater than "<<a;
+	 
+	return 1;
+	
+}
